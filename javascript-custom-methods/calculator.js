974df4930687819234266040739e2c80beb096a1
Owen Adams <owenadams.main@gmail.com>
2022-05-10 02:41:19 +0000

    create methods of the calculator object. complete all tests. ready for review

--- javascript-custom-methods/calculator.js
+++ javascript-custom-methods/calculator.js
@@ -1,1 +1,33 @@
 /* exported calculator */
+
+var calculator = {
+  add: function (x, y) {
+    return x + y;
+  },
+  subtract: function (x, y) {
+    return x - y;
+  },
+  multiply: function (x, y) {
+    return x * y;
+  },
+  divide: function (x, y) {
+    return x / y;
+  },
+  square: function (x) {
+    return x * x;
+  },
+  sumAll: function (array) {
+    let result = 0;
+    for (let numIndex = 0; numIndex < array.length; numIndex++) {
+      result += array[numIndex];
+    }
+    return result;
+  },
+  getAverage: function (array) {
+    let result = 0;
+    for (let numIndex = 0; numIndex < array.length; numIndex++) {
+      result += array[numIndex];
+    }
+    return result / array.length;
+  }
+};
