934f11418bb3e03def7bf0f153c7d2db9bb12df1
Owen Adams <owenadams.main@gmail.com>
2022-05-10 15:05:01 +0000

    add object with function. add array with warner values, add function to loop and add prototype to warner variables. call function. ready for review

--- javascript-prototypes/warner-bros.js
+++ javascript-prototypes/warner-bros.js
@@ -3,32 +3,35 @@
 var yakko = {
   age: 14,
   name: 'Yakko',
-  role: 'brother',
-  describe: function () {
-    var description = this.name + ' is a ' +
-                      this.age + '-year-old Warner ' + this.role + '.';
-    return description;
-  }
+  role: 'brother'
 };
 
 var wakko = {
   age: 11,
   name: 'Wakko',
-  role: 'brother',
-  describe: function () {
-    var description = this.name + ' is a ' +
-                      this.age + '-year-old Warner ' + this.role + '.';
-    return description;
-  }
+  role: 'brother'
 };
 
 var dot = {
   age: 10,
   name: 'Dot',
-  role: 'sister',
+  role: 'sister'
+};
+
+const warnerPrototype = {
   describe: function () {
-    var description = this.name + ' is a ' +
-                      this.age + '-year-old Warner ' + this.role + '.';
+    const description = this.name + ' is a ' +
+      this.age + '-year-old Warner ' + this.role + '.';
     return description;
   }
 };
+
+const warnerFamily = [yakko, wakko, dot];
+
+function addWarnerPrototype() {
+  for (let i = 0; i < warnerFamily.length; i++) {
+    Object.setPrototypeOf(warnerFamily[i], warnerPrototype);
+  }
+}
+
+addWarnerPrototype();
