0f114172ccfdf32826a2dbeffd5cc8dd6b3107fe
Owen Adams <owenadams.main@gmail.com>
2022-05-10 03:28:51 +0000

    add object with methods. all tests passing. test in console. ready for review

--- javascript-this/student.js
+++ javascript-this/student.js
@@ -1,1 +1,11 @@
 /* exported student */
+
+const student = {
+  firstName: 'Bruce',
+  lastName: 'Willis',
+  subject: 'Terrorist Destroying',
+  getFullName: function () { return this.firstName + ' ' + this.lastName; },
+  getIntroduction: function () {
+    return 'Hello, my name is ' + this.firstName + ' ' + this.lastName + ' and I am studying ' + this.subject + '.';
+  }
+};
