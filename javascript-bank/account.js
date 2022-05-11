61e99fb9312a96fb6ff80d3b1fcd865ad41e6fdb
Owen Adams <owenadams.main@gmail.com>
2022-05-11 04:26:43 +0000

    add prototype deposit to account, add functionality. WIP

--- javascript-bank/account.js
+++ javascript-bank/account.js
@@ -1,1 +1,15 @@
 /* exported Account */
+function Account(number, holder) {
+  this.number = number;
+  this.holder = holder;
+  this.transactions = [];
+}
+Account.prototype.deposit = function (amount) {
+  if (!Number.isInteger(amount) || amount <= 0) {
+    return false;
+  } else if (Number.isInteger(amount) && amount > 0) {
+    const depositTransaction = new Transaction('deposit', amount);
+    this.transactions.push(depositTransaction);
+    return true;
+  }
+};
