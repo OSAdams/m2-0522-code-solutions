24437f19b8f6c3943f841367d7df11bd09ff9259
Owen Adams <owenadams.main@gmail.com>
2022-05-11 02:36:14 +0000

    add constructor with 3 parameters. assign argument values to this.variables. add two prototypes to the Student object which return a string value. Ready for review

--- javascript-constructors/student.js
+++ javascript-constructors/student.js
@@ -1,1 +1,18 @@
 /* exported Student */
+
+function Student(firstName, lastName, subject) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.subject = subject;
+}
+
+Student.prototype.getFullName = function () {
+  this.fullName = this.firstName + ' ' + this.lastName;
+  return this.fullName;
+};
+
+Student.prototype.getIntroduction = function () {
+  this.introduction = 'Hello, my name is ' + this.getFullName() +
+   ' and I am studying ' + this.subject + '.';
+  return this.introduction;
+};
