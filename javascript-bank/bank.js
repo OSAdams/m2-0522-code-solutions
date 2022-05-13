703783a9a5ee10bc5553dfe237c1d392e9874290
Owen Adams <owenadams.main@gmail.com>
2022-05-13 03:04:40 +0000

    add two prototypes to the bank object. WIP

--- javascript-bank/bank.js
+++ javascript-bank/bank.js
@@ -1,1 +1,28 @@
 /* exported Bank */
+
+function Bank() {
+  this.nextAccountNumber = 1;
+  this.accounts = [];
+}
+
+Bank.prototype.openAccount = function (holder, balance) {
+  if (!Number.isInteger(balance) || balance < 0 || !balance) {
+    return null;
+  }
+  if (balance > 0) {
+    const newAccount = new Account(this.nextAccountNumber, holder);
+    newAccount.deposit(balance);
+    this.accounts.push(newAccount);
+    this.nextAccountNumber++;
+    return newAccount.number;
+  }
+};
+
+Bank.prototype.getAccount = function (number) {
+  for (let accountIndex = 0; accountIndex < this.accounts.length; accountIndex++) {
+    if (this.accounts[accountIndex].number === number) {
+      return this.accounts[accountIndex];
+    }
+  }
+  return null;
+};
