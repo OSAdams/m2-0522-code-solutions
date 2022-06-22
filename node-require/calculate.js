7499bad13ab1949a42a556ee102eac7cb550c5eb
Owen Adams <owenadams.main@gmail.com>
2022-06-22 17:00:00 +0000

    imported add, subtract, multiply, divide. define function, call function. ready for review

---
+++ node-require/calculate.js
@@ -0,0 +1,23 @@
+const add = require('./add');
+const subtract = require('./subtract');
+const multiply = require('./multiply');
+const divide = require('./divide');
+
+const firstNumber = parseInt(process.argv[2]);
+const secondNumber = parseInt(process.argv[4]);
+
+function findOperand(string) {
+  if (string === 'plus') {
+    console.log('Result: ', add.add(firstNumber, secondNumber));
+  } else if (string === 'minus') {
+    console.log('Result: ', subtract.subtract(firstNumber, secondNumber));
+  } else if (string === 'times') {
+    console.log('Result: ', multiply.multiply(firstNumber, secondNumber));
+  } else if (string === 'over') {
+    console.log('Result: ', divide.divide(firstNumber, secondNumber));
+  } else {
+    console.log('Invalid Operation');
+  }
+}
+
+findOperand(process.argv[3]);
