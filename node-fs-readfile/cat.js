8cb064096f716cb5d7a9dc86a851acea82450864
Owen Adams <owenadams.main@gmail.com>
2022-06-25 16:26:25 +0000

    comments please read

--- node-fs-readfile/cat.js
+++ node-fs-readfile/cat.js
@@ -10,3 +10,28 @@ nodeArgs.forEach(file => {
     console.log(data);
   });
 });
+
+// !!! I googled and found a forEach method but it felt like cheating.
+// Below is what I came up with, after trial and error using different kinds of
+// loops. It works, but is obviously limited to how many times I want to call the
+// readFile method of the fs object. Anyway, even if approved, if you could share
+// a hint to another solution I'm all ears. This was frustrating and fun.
+
+// fs.readFile(process.argv[2], 'utf8', (err, data) => {
+//   if (err) {
+//     console.error(err);
+//   }
+//   console.log(data);
+//   fs.readFile(process.argv[3], 'utf8', (err, data) => {
+//     if (err) {
+//       console.error(err);
+//     }
+//     console.log(data);
+//     fs.readFile(process.argv[4], 'utf8', (err, data) => {
+//       if (err) {
+//         console.error(err);
+//       }
+//       console.log(data);
+//     });
+//   });
+// });
