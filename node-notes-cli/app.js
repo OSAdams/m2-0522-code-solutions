6a875eb946258e333b57817acc4f4dbbd95cb553
Owen Adams <owenadams.main@gmail.com>
2022-06-28 04:29:23 +0000

    complete update feature of challenge

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -31,4 +31,28 @@ if (keyWord === 'read') {
       }
     });
   });
+} else if (keyWord === 'update') {
+  fs.readFile('data.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err.message);
+    }
+    const notesObj = JSON.parse(data);
+    if (isNaN(Number(process.argv[3]))) {
+      console.error({ error: 'id must be a number' });
+      return;
+    } else if (!notesObj.notes[process.argv[3]]) {
+      console.error({ error: 'id does not exist' });
+      return;
+    }
+    for (const notesObjId in notesObj.notes) {
+      if (notesObjId === process.argv[3]) {
+        notesObj.notes[notesObjId] = process.argv[4];
+      }
+    }
+    fs.writeFile('data.json', JSON.stringify(notesObj, null, 2), 'utf8', err => {
+      if (err) {
+        console.error(err.message);
+      }
+    });
+  });
 }
