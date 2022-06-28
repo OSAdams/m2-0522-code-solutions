7f6c95096b5a23fcb4b080d353178f7f5c06fe93
Owen Adams <owenadams.main@gmail.com>
2022-06-28 06:16:08 +0000

    add errors, complete delete feature. thorough test. ready for review

--- node-notes-cli/app.js
+++ node-notes-cli/app.js
@@ -1,58 +1,95 @@
 const fs = require('fs');
 
 const keyWord = process.argv[2];
+const keyWordLib = ['read', 'create', 'update', 'delete'];
 
-if (keyWord === 'read') {
-  fs.readFile('data.json', 'utf8', (err, data) => {
-    if (err) {
-      console.error('error: ', err.message);
-    }
-    const notesObj = JSON.parse(data);
-    for (const notesId in notesObj.notes) {
-      console.log(notesId + ': ' + notesObj.notes[notesId]);
-    }
-  });
-} else if (keyWord === 'create') {
-  fs.readFile('data.json', 'utf8', (err, data) => {
-    if (err) {
-      console.error('error: ', err.message);
-    }
-    const notesObj = JSON.parse(data);
-    const newId = notesObj.nextId;
-    if (!process.argv[3]) {
-      console.error({ error: 'invalid argument. please input a string' });
-      return;
-    }
-    notesObj.notes[newId] = process.argv[3];
-    notesObj.nextId++;
-    fs.writeFile('data.json', JSON.stringify(notesObj, null, 2), 'utf8', err => {
+if (!keyWordLib.includes(keyWord)) {
+  console.error({ error: 'invalid command' });
+} else if (keyWordLib.includes(keyWord)) {
+  if (keyWord === keyWordLib[0]) {
+    fs.readFile('data.json', 'utf8', (err, data) => {
       if (err) {
-        console.error(err.message);
+        console.error(err);
+      }
+      const notesObj = JSON.parse(data);
+      for (const notesId in notesObj.notes) {
+        console.log(notesId + ': ' + notesObj.notes[notesId]);
       }
     });
-  });
-} else if (keyWord === 'update') {
-  fs.readFile('data.json', 'utf8', (err, data) => {
-    if (err) {
-      console.error(err.message);
-    }
-    const notesObj = JSON.parse(data);
-    if (isNaN(Number(process.argv[3]))) {
-      console.error({ error: 'id must be a number' });
-      return;
-    } else if (!notesObj.notes[process.argv[3]]) {
-      console.error({ error: 'id does not exist' });
-      return;
-    }
-    for (const notesObjId in notesObj.notes) {
-      if (notesObjId === process.argv[3]) {
-        notesObj.notes[notesObjId] = process.argv[4];
-      }
-    }
-    fs.writeFile('data.json', JSON.stringify(notesObj, null, 2), 'utf8', err => {
+  } else if (keyWord === keyWordLib[1]) {
+    fs.readFile('data.json', 'utf8', (err, data) => {
       if (err) {
-        console.error(err.message);
+        console.error(err);
       }
+      const notesObj = JSON.parse(data);
+      const newId = notesObj.nextId;
+      if (!process.argv[3]) {
+        console.error({ error: 'invalid argument. please input a string' });
+        return;
+      }
+      notesObj.notes[newId] = process.argv[3];
+      notesObj.nextId++;
+      console.log(`Note ID ${newId} successfully created`);
+      fs.writeFile('data.json', JSON.stringify(notesObj, null, 2), 'utf8', err => {
+        if (err) {
+          console.error(err);
+        }
+      });
     });
-  });
+  } else if (keyWord === keyWordLib[2]) {
+    const inputId = process.argv[3];
+    fs.readFile('data.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      }
+      const notesObj = JSON.parse(data);
+      if (isNaN(Number(inputId)) || Math.floor(inputId) !== Number(inputId)) {
+        console.error({ error: 'id must be a whole number' });
+        return;
+      } else if (!notesObj.notes[inputId]) {
+        console.error({ error: 'id does not exist' });
+        return;
+      } else if (!process.argv[4]) {
+        console.error({ error: 'invalid argument. please input a string' });
+        return;
+      }
+      for (const notesObjId in notesObj.notes) {
+        if (notesObjId === inputId) {
+          notesObj.notes[notesObjId] = process.argv[4];
+          console.log(`Note ID ${notesObjId} successfully updated`);
+        }
+      }
+      fs.writeFile('data.json', JSON.stringify(notesObj, null, 2), 'utf8', err => {
+        if (err) {
+          console.error(err);
+        }
+      });
+    });
+  } else if (keyWord === keyWordLib[3]) {
+    const inputId = process.argv[3];
+    fs.readFile('data.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      }
+      const notesObj = JSON.parse(data);
+      if (isNaN(Number(inputId)) || Math.floor(inputId) !== Number(inputId)) {
+        console.error({ error: 'id must be a whole number' });
+        return;
+      } else if (!notesObj.notes[inputId]) {
+        console.error({ error: 'id does not exist' });
+        return;
+      }
+      for (const notesObjId in notesObj.notes) {
+        if (notesObjId === inputId) {
+          delete notesObj.notes[notesObjId];
+          console.log(`Note ID ${notesObjId} successfully deleted`);
+        }
+      }
+      fs.writeFile('data.json', JSON.stringify(notesObj, null, 2), 'utf8', err => {
+        if (err) {
+          console.error(err);
+        }
+      });
+    });
+  }
 }
