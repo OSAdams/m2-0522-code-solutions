5d6fd758789b0769d35df99e38574ba0d51af0bb
Owen Adams <owenadams.main@gmail.com>
2022-07-12 06:04:56 +0000

    copy starter files to branch. update definition of callback function being called in the get method of the app object

---
+++ express-notes-api/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const jsonData = require('./data.json');
+const app = express();
+
+// eslint-disable-next-line
+app.listen(3000, () => { console.log('Express server listening on port 3000'); });
+
+app.use(express.json());
+
+app.get('/api/notes/', (req, res) => {
+  const notes = [];
+  for (const id in jsonData.notes) {
+    notes.push(jsonData.notes[id]);
+  }
+  res.status(200).json(notes);
+});
