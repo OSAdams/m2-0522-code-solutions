8cac6ddb483ec92c498283903e9082ed2a42a584
Owen Adams <owenadams.main@gmail.com>
2022-07-25 21:54:40 +0000

    complete exercise. ready for review

--- es6-promises/index.js
+++ es6-promises/index.js
@@ -1,1 +1,9 @@
 const takeAChance = require('./take-a-chance');
+
+const returnValue = takeAChance('Who Dat');
+
+returnValue.then(message => {
+  console.log(message);
+}).catch(error => {
+  console.error(error.message);
+});
